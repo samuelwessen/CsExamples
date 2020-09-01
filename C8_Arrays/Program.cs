1915627ee1dc572d9bd3b1a1904e29612f896942
Samuel Wessén <SamuelWessen@hotmail.com>
2020-09-01 15:02:07 +0200

    C8

---
+++ C8_Arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace C8_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("How many siblings do you have? ");
+            var _numberOfSiblings = Convert.ToInt32(Console.ReadLine());
+
+            if (_numberOfSiblings < 1)
+                Console.WriteLine("Okay. That´s cool man.");
+            else
+            {
+                string[] siblings = new string[_numberOfSiblings];
+
+                if (_numberOfSiblings == 1)
+                {
+                    Console.Write("What´s your siblings name?");
+                    siblings[0] = Console.ReadLine();
+                }
+                else
+                {
+                    for (var i=0; i< _numberOfSiblings; i++)
+                    {
+                        if (i == 0)
+                            Console.Write("What´s your first siblings name? ");
+                        else
+                            Console.Write("And your next siblings name is? ");
+
+                        siblings[i] = Console.ReadLine();
+                    }
+
+                }
+
+
+                if (_numberOfSiblings == 1)
+                    Console.Write("So correct me if I am wrong. Yor sibling is ");
+                else
+                    Console.Write("So correct me if I am wrong. Yor siblings is ");
+
+                foreach(var sibling in siblings) 
+                {
+                    Console.Write($"{sibling} ");
+                }
+
+
+            }
+
+        }
+    }
+}
