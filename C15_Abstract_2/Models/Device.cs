4ed790f57e06a1e08a7ad0d48776614a81731b66
Samuel Wessén <SamuelWessen@hotmail.com>
2020-09-03 16:38:53 +0200

    C14

---
+++ C15_Abstract_2/Models/Device.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C15_Abstract_2.Models
+{
+    abstract class Device
+    {
+        public virtual string CreateFromConnectionString(string connectionstring)
+        {
+            return $"Initializing connetion with \"{connectionstring}\"";
+        }
+
+        public abstract string ReceiveMessage();
+        public abstract void SendMessage(string payload);
+
+    }
+}
