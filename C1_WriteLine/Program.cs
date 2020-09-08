776a50bdc17ebfa8711b5582ead58611850e1567
Samuel Wessén <SamuelWessen@hotmail.com>
2020-09-08 17:01:19 +0100

    C17

--- C1_WriteLine/Program.cs
+++ C1_WriteLine/Program.cs
@@ -6,7 +6,7 @@ namespace C1_WriteLine
     {
         static void Main(string[] args)
         {
-            string name = "Samuell";
+            string name = "Samuel";
             int age = 34;
             var city = "Örebro";
 
